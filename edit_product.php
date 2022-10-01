3839644f73fa2bdd17b1eb60ddbd026e7c71996b
ZeroneDoo <rizqipambudi6@gmail.com>
2022-10-01 17:00:55 +0700

    edit

--- edit_product.php
+++ edit_product.php
@@ -19,7 +19,7 @@ if(mysqli_num_rows($tampil)<1){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Edit</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -45,7 +45,7 @@ if(mysqli_num_rows($tampil)<1){
                         Sebelumnya : <?= $data['img']?>
                     </label>
                 <div class="co">
-                    <input type="text" placeholder="$XXX.XX" name="harga" value="$ <?= $data['harga']?>" id="">  
+                    <input type="text" placeholder="$ XXX.XX" name="harga" value=" <?= $data['harga']?>" id="">  
                     <input type="text" placeholder="Title..." name="judul"  value="<?= $data['judul']?>">                  
                     <textarea placeholder="input here..." name="deskripsi" id=""><?= $data['deskripsi']?></textarea>
                 </div>
